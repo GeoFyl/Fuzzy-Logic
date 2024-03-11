6cfe9c15fdfb2ae8797e36121d672e6ba074f78c
GEORGE FYLES <2100707@uad.ac.uk>
2024-03-11 02:50:20 +0000

    began fuzzy logic idea

---
+++ MAT301 Project/Assets/Scripts/Fuzzy Missile.cs
@@ -0,0 +1,95 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using FLS;
+using FLS.Rules;
+using FLS.MembershipFunctions;
+
+public class FuzzyMissile : MonoBehaviour {
+
+	bool selected = false;
+	GameObject player_;
+
+	IFuzzyEngine sideways_engine_, forward_engine_;
+	LinguisticVariable sideways_distance_, forward_distance_, sideways_direction_, forward_direction_; 
+
+	void Start()
+	{
+		player_ = GameObject.FindWithTag("Player");
+
+		// ---------- Setup Fuzzy Inference System -----------
+
+		// Engine for sideways movement (x axis)
+		sideways_engine_ = new FuzzyEngineFactory().Default();
+
+		sideways_distance_ = new LinguisticVariable("sideways_distance");
+		var left_of_target = sideways_distance_.MembershipFunctions.AddTrapezoid("left", -50, -50, -5, -1);
+        var inline_with_target = sideways_distance_.MembershipFunctions.AddTrapezoid("none", -5, -0.5, 0.5, 5);
+        var right_of_target = sideways_distance_.MembershipFunctions.AddTrapezoid("right", 1, 5, 50, 50);
+        sideways_direction_ = new LinguisticVariable("sideways_direction");
+		var steer_left = sideways_direction_.MembershipFunctions.AddTrapezoid("left", -50, -50, -5, -1);
+        var stay_centred = sideways_direction_.MembershipFunctions.AddTrapezoid("none", -5, -0.5, 0.5, 5);
+        var steer_right = sideways_direction_.MembershipFunctions.AddTrapezoid("right", 1, 5, 50, 50);
+        var sideways_rule_1 = Rule.If(sideways_distance_.Is(right_of_target)).Then(sideways_direction_.Is(steer_left));
+        var sideways_rule_2 = Rule.If(sideways_distance_.Is(left_of_target)).Then(sideways_direction_.Is(steer_right));
+        var sideways_rule_3 = Rule.If(sideways_distance_.Is(inline_with_target)).Then(sideways_direction_.Is(stay_centred));
+        sideways_engine_.Rules.Add(sideways_rule_1, sideways_rule_2, sideways_rule_3);
+
+        // Engine for forward direction movement (z axis)
+        forward_engine_ = new FuzzyEngineFactory().Default();
+
+        forward_distance_ = new LinguisticVariable("forward_distance");
+		var behind_target = forward_distance_.MembershipFunctions.AddTrapezoid("behind1", -50, -50, -5, -1);
+        var alongside_target = forward_distance_.MembershipFunctions.AddTrapezoid("none1", -5, -0.5, 0.5, 5);
+        var in_front_of_target = forward_distance_.MembershipFunctions.AddTrapezoid("infront1", 1, 5, 50, 50);
+        forward_direction_ = new LinguisticVariable("forward_direction");
+		var move_back = forward_direction_.MembershipFunctions.AddTrapezoid("back1", -50, -50, -5, -1);
+        var stay_alongside = forward_direction_.MembershipFunctions.AddTrapezoid("none1", -5, -0.5, 0.5, 5);
+        var move_forward = forward_direction_.MembershipFunctions.AddTrapezoid("forward1", 1, 5, 50, 50);
+		var forward_rule_1 = Rule.If(forward_distance_.Is(in_front_of_target)).Then(forward_direction_.Is(move_back));
+		var forward_rule_2 = Rule.If(forward_distance_.Is(behind_target)).Then(forward_direction_.Is(move_forward));
+		var forward_rule_3 = Rule.If(forward_distance_.Is(alongside_target)).Then(forward_direction_.Is(stay_alongside));
+		forward_engine_.Rules.Add(forward_rule_1, forward_rule_2, forward_rule_3);
+
+    }
+
+	void FixedUpdate()
+	{
+        if (!selected)
+		{
+			// Convert position of box to value between 0 and 100
+			double sideways_result = sideways_engine_.Defuzzify(new { sideways_distance = (double)transform.position.x - player_.transform.position.x});
+			double forward_result = forward_engine_.Defuzzify(new { forward_distance = (double)transform.position.z - player_.transform.position.z});
+
+			Rigidbody rigidbody = GetComponent<Rigidbody>();
+			rigidbody.AddForce(new Vector3((float)sideways_result, 0f, (float)forward_result));
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetMouseButtonDown(0)) {
+			RaycastHit hit;
+			var ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+			if (Physics.Raycast(ray, out hit)){
+				if (hit.rigidbody.gameObject == gameObject)
+				{
+					selected = true;
+				}
+			}
+		}
+
+		if(Input.GetMouseButton(0) && selected)
+		{
+			float distanceToScreen = Camera.main.WorldToScreenPoint(gameObject.transform.position).z;
+			Vector3 curPosition = Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, distanceToScreen));
+			transform.position = new Vector3(curPosition.x, Mathf.Max(0.5f, curPosition.y), transform.position.z);
+		}
+
+		if(Input.GetMouseButtonUp(0))
+		{
+			selected = false;
+		}
+	}
+}
--- MAT301 Project/Assets/Scripts/Reinforcement Learning.cs
+++ MAT301 Project/Assets/Scripts/Reinforcement Learning.cs
@@ -13,7 +13,8 @@ public class ReinforcementLearning : MonoBehaviour
     enum State { OBSTACLE_IN_FRONT, GOAL_IN_FRONT, NONE_IN_FRONT };
     enum Action { MOVE_FORWARD, ROTATE_RIGHT, ROTATE_LEFT };
     State previous_state_ = State.NONE_IN_FRONT;
-    Action chosen_action_;
+    int index_ = -1;
+    float best_value_;
 
     float[,] Q = { { 0, 0, 0 }, { 0, 0, 0 }, { 0, 0, 0 } };
     String[] action_to_string_ = new String[3] { "Move Forward", "Rotate Right", "Rotate Left" };
@@ -37,6 +38,9 @@ public class ReinforcementLearning : MonoBehaviour
     bool reached_goal_ = false;
     GameObject goal_;
     GameObject current_obstacle_;
+    float distance_to_goal_;
+
+    float time_ = 0;
 
     void printMatrix()
     {
@@ -64,6 +68,7 @@ public class ReinforcementLearning : MonoBehaviour
     void Start()
     {
         goal_ = GameObject.Find("Goal");
+        distance_to_goal_ = (transform.position - goal_.transform.position).magnitude;
     }
 
     // Update is called once per frame
@@ -71,111 +76,132 @@ public class ReinforcementLearning : MonoBehaviour
     {
         if (!reached_goal_)
         {
-            // Describe State
-            State current_state = GetState();
+           // time_ += Time.deltaTime;
+            //if (time_ >= 0.2)
+           // {
+           //     time_ = 0;
 
-            // Choose Action
-            float best_value = Q[(int)current_state, 0];
-            int index = 0;
-            for (int i = 0; i < num_actions_; i++)
-            {
-                if (Q[(int)current_state, i] > best_value)
+                // Describe State
+                State new_state = GetState();
+
+                if (index_ > -1) // Can't do this the first time
                 {
-                    best_value = Q[(int)current_state, i];
-                    index = i;
+                    // Get reward for previous action
+                    float reward = CalculateReward(new_state);
+
+                    // Print out which action the AI ran.
+                    Debug.Log(string.Format("State was: {0} Ran action: {1}, Reward recieved: {2}", state_to_string_[(int)previous_state_], action_to_string_[index_], reward));
+
+                    // Update Q Matrix
+                    Q[(int)previous_state_, index_] += learning_rate_ * (reward + discounting_rate_ * best_value_ - Q[(int)previous_state_, index_]);
                 }
-            }
-            if (Random.Range(0, 99) < random_chance_)
-            {
-                index = Random.Range(0, num_actions_);
-            }
-            Action chosen_action = (Action)index;
+                previous_state_ = new_state;
 
-            // Perform the Action
-            float distance_to_goal = (transform.position - goal_.transform.position).magnitude;
-            switch (chosen_action)
-            {
-                case Action.MOVE_FORWARD:
-                   // transform.Translate(Vector3.forward * 5 * Time.deltaTime);
-                    GetComponent<Rigidbody>().velocity = transform.forward * 5;
-                    break;
-                case Action.ROTATE_LEFT:
-                    GetComponent<Rigidbody>().velocity = Vector3.zero;
-                    transform.Rotate(-Vector3.up * rotate_angles_);
-                    break;
-                case Action.ROTATE_RIGHT:
-                    GetComponent<Rigidbody>().velocity = Vector3.zero;
-                    transform.Rotate(Vector3.up * rotate_angles_);
-                    break;
-                default: break;
-            }
-
-            // Calculate Reward
-            float reward = 0;
-            State new_state = GetState();
-           
-            if (new_state != current_state)
-            {
-                switch (current_state)
+                // Choose Action
+                best_value_ = Q[(int)new_state, 0];
+                index_ = 0;
+                for (int i = 0; i < num_actions_; i++)
                 {
-                    case State.OBSTACLE_IN_FRONT:
-                        reward += 5;
+                    if (Q[(int)new_state, i] > best_value_)
+                    {
+                        best_value_ = Q[(int)new_state, i];
+                        index_ = i;
+                    }
+                }
+                if (Random.Range(0, 99) < random_chance_)
+                {
+                    index_ = Random.Range(0, num_actions_);
+                }
+                Action chosen_action = (Action)index_;
+
+                // Perform the Action
+                switch (chosen_action)
+                {
+                    case Action.MOVE_FORWARD:
+                        // transform.Translate(Vector3.forward * 5 * Time.deltaTime);
+                        GetComponent<Rigidbody>().velocity = transform.forward * 5;
                         break;
-                    case State.GOAL_IN_FRONT:
-                        reward -= 10;
+                    case Action.ROTATE_LEFT:
+                        GetComponent<Rigidbody>().velocity = Vector3.zero;
+                        transform.Rotate(-Vector3.up * rotate_angles_);
+                        break;
+                    case Action.ROTATE_RIGHT:
+                        GetComponent<Rigidbody>().velocity = Vector3.zero;
+                        transform.Rotate(Vector3.up * rotate_angles_);
                         break;
                     default: break;
                 }
-                switch (new_state)
-                {
-                    case State.GOAL_IN_FRONT:
-                        reward += 10;
-                        break;
-                    case State.OBSTACLE_IN_FRONT:
-                        reward -= 5;
-                        break;
-                }
-            }
-            else
-            {
-                switch (current_state)
-                {
-                    case State.OBSTACLE_IN_FRONT:
-                        reward -= 10;
-                        //Vector3.Dot(transform.forward, )
-                       // transform.forward.
-                        break;
-                    case State.GOAL_IN_FRONT:
-                        reward += 5;
-                        break;
-                    case State.NONE_IN_FRONT:
-                        reward += 1;
-                        break;
-                }
-            }
-            float new_distance = (transform.position - goal_.transform.position).magnitude;
-            if (new_distance < distance_to_goal) reward *= 100f * (distance_to_goal - new_distance);
-            else if (new_distance > distance_to_goal) reward *= -100f * (new_distance - distance_to_goal); 
 
-            // Update Q Matrix
-            Q[(int)current_state, index] += learning_rate_ * (reward + discounting_rate_ * best_value - Q[(int)current_state, index]);
+            //}
 
-            // Print out which action the AI ran.
-            //Debug.Log(string.Format("State was: {0} Ran action: {1}, Reward recieved: {2}", state_to_string_[(int)current_state], action_to_string_[index], reward));
+            //if (Input.GetKey(KeyCode.W))
+            //{
+            //    transform.Translate(Vector3.forward * 5 * Time.deltaTime);
+            //}
+            //else if (Input.GetKey(KeyCode.Q))
+            //{
+            //    transform.Rotate(-Vector3.up * 30 * Time.deltaTime);
+            //}
+            //else if (Input.GetKey(KeyCode.E))
+            //{
+            //    transform.Rotate(Vector3.up * 30 * Time.deltaTime);
+            //}
         }
+    }
 
-        //if (Input.GetKey(KeyCode.W))
-        //{
-        //    transform.Translate(Vector3.forward * 5 * Time.deltaTime);
-        //}
-        //else if (Input.GetKey(KeyCode.Q))
-        //{
-        //    transform.Rotate(-Vector3.up * 30 * Time.deltaTime);
-        //}
-        //else if (Input.GetKey(KeyCode.E))
-        //{
-        //    transform.Rotate(Vector3.up * 30 * Time.deltaTime);
-        //}
+    float CalculateReward(State new_state)
+    {
+        float reward = 0;
+        if (new_state != previous_state_)
+        {
+            switch (previous_state_)
+            {
+                case State.OBSTACLE_IN_FRONT:
+                    reward += 5;
+                    break;
+                case State.GOAL_IN_FRONT:
+                    reward -= 10;
+                    break;
+                default: break;
+            }
+            switch (new_state)
+            {
+                case State.GOAL_IN_FRONT:
+                    reward += 10;
+                    break;
+                case State.OBSTACLE_IN_FRONT:
+                    reward -= 5;
+                    break;
+                default: break;
+            }
+        }
+        else
+        {
+            switch (new_state)
+            {
+                case State.OBSTACLE_IN_FRONT:
+                    reward -= 10;
+                    //Vector3.Dot(transform.forward, )
+                    // transform.forward.
+                    break;
+                case State.GOAL_IN_FRONT:
+                    reward += 5;
+                    break;
+                case State.NONE_IN_FRONT:
+                    reward += 1;
+                    break;
+                default: break;
+            }
+        }
+        float new_distance = (transform.position - goal_.transform.position).magnitude;
+        if (new_distance < distance_to_goal_) reward += 100f * (distance_to_goal_ - new_distance);
+        else if (new_distance > distance_to_goal_) reward -= 100f * (new_distance - distance_to_goal_);
+
+        //Debug.Log("old dist: " + distance_to_goal_ + ", new dist: " + new_distance);
+
+        distance_to_goal_ = new_distance;
+
+        return reward;
     }
 
     State GetState()
